810cfa34248fa59740e3f2be2f345ebfe3a54864
Hardik Desai <hp676913@gmail.com>
2022-07-23 17:56:58 +0530

    All Project Work Done

--- assets/js/Project.js
+++ assets/js/Project.js
@@ -1,13 +1,34 @@
 let projectBox = document.getElementById('projectBox');
-let colorArr = ["primary","success","danger","warning"];
-let language = ["CSS", "Android","HTML","JavaScript"]
+let langColor = {
+    "HTML": "text-white bg-danger",
+    "CSS": "text-white bg-primary",
+    "Javascript": "text-black bg-warning",
+    "Android": "text-white bg-success",
+    "API": "text-black bg-info",
+    "Bootstrap": "text-white bg-primary",
+    "Firebase": "text-black bg-warning",
+    "JAVA": "text-white bg-secondary"
+}
+
 function showProjects() {
     fetch('assets/json/project.json')
         .then(response => response.json())
         .then(data => {
             let str = "";
             let projects = data.projects;
+
             for (key in projects) {
+
+                function returnBadgeHTML() {
+                    let htm = "";
+
+                    let Language = projects[key].badge;
+                    for (index in Language) {
+                        htm += `<span class="langBadge badge rounded-pill ${langColor[Language[index]]}">${Language[index]}</span>`;
+                    }
+                    return htm;
+                }
+
                 str += ` 
                             <div class="col-sm-6 col-lg-4 item">
                                 <img class="imgThumbnail img-fluid rounded-0 border" data-bss-hover-animate="pulse"
