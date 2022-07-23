eda0c62244302a91dfb4da9b92aa657243e90a81
Hardik Desai <hp676913@gmail.com>
2022-07-23 15:11:24 +0530

    Add images

--- assets/js/Project.js
+++ assets/js/Project.js
@@ -7,11 +7,16 @@ function showProjects() {
             let str = "";
             let projects = data.projects;
             for (key in projects) {
-                str += `<div class="col-sm-6 col-lg-4 item"><img class="img-fluid rounded-0 border"
-                            data-bss-hover-animate="pulse" src="assets/img/desk.jpg">
-                        <h3 class="name">${projects[key].title}</h3>
-                        <p class="description">${projects[key].discription}</p>
-                        </div>`;
+                str += ` 
+                            <div class="col-sm-6 col-lg-4 item">
+                                <img class="imgThumbnail img-fluid rounded-0 border" data-bss-hover-animate="pulse"
+                                    src="${projects[key].imgurl}">
+                                <h3 class="name">${projects[key].title}</h3>
+                                <p class="description">${projects[key].discription}</p>
+                                <a href="/" target="_blank"><button type="button" class="readBtn btn btn-outline-primary">Read
+                                more</button></a>
+                            </div>
+                        `;
             }
             projectBox.innerHTML = str;
         })
