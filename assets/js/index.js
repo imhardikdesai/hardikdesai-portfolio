261c4a3f913f0173948cbbfc618bc553acfc26e2
imhardikdesai <hp676913@gmail.com>
2022-09-10 17:39:21 +0530

    changes in index.html responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,23 +5,24 @@ function showHomePageProjects() {
         .then(data => {
             let projects = data.projects;
             let str = ""
-            for (let i = 0; i <4; i++) {
+            for (let i = 0; i < 4; i++) {
                 str += `
                             <div class="col-sm-6 item">
                                 <div class="row homepageProjects">
-                                    <div class="col-md-12 col-lg-5">
-                                        <a href="#"><img class="imgThumbnail img-fluid" data-aos="fade" data-aos-duration="1700"
-                                                src="${projects[i].imgurl}" /></a>
+                                    <div class="col-md-12 col-lg-5 d-flex justify-content-center align-content-center">
+                                        <a href="#">
+                                            <img class="imgThumbnail img-fluid" data-aos="fade" data-aos-duration="1700" src="${projects[i].imgurl}" />
+                                        </a>
                                     </div>
-                                    <div class="col">
+                                    <div class="col text-center">
                                         <h3 class="name">${projects[i].title}</h3>
                                         <p class="description">${projects[i].discription}</p>
-
-                                        <a href="${projects[i].liveDemoLink}" target="_blank"><button type="button"
-                                                class="readBtn btn btn-outline-primary btn-sm">Live Demo</button></a>
-
-                                        <a href="${projects[i].sourceLink}" target="_blank"><button type="button"
-                                                class="readBtn btn btn-outline-primary btn-sm">Source Code</button></a>
+                                        <a href="${projects[i].liveDemoLink}" target="_blank">
+                                            <button type="button" class="readBtn btn btn-outline-primary btn-sm">Live Demo</button>
+                                        </a>
+                                        <a href="${projects[i].sourceLink}" target="_blank">
+                                            <button type="button" class="readBtn btn btn-outline-primary btn-sm">Source Code</button>
+                                        </a>
                                     </div>
                                 </div>
                              </div>
